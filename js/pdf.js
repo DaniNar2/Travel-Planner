5a3a1c244ca0800432a685a504b9bf26b025a5e6
Daniela Nardone <dani.uni1947@gmail.com>
2026-03-16 23:17:58 +0100

    Modifiche importanti
    Almeno mo funziona

--- js/pdf.js
+++
@@ -1,7 +0,0 @@
-function downloadPDF(){
-
-let element=document.body
-
-html2pdf().from(element).save("planning.pdf")
-
-}
--- js/storage.js
+++ js/storage.js
@@ -1,14 +1,10 @@
 function getPlannings(){
-
 let data = localStorage.getItem("plannings")
-
 if(data == null){
 return []
 }
-
 return JSON.parse(data)
 }
-
 function savePlannings(plannings){
 localStorage.setItem("plannings", JSON.stringify(plannings))
 }
